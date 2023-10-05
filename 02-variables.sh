6bb903610738556aeaa35bee76412b92c3b18f60
vikram Reddy <vikram@gmail.com>
2023-10-05 11:25:07 -0400

    updated conversation  Ramesh and suresh

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
-# shell scrip will run the command inside the brackets and store inside the variable called dDATE
-DATE = $(date)
-echo "Hello , this scripted is executed at time : $DATE
+#!/bin/bash
+#shell scrip will run the command inside the brackets and store inside the variable called dDATE
+DATE=$(date)
+echo "Hello , this scripted is executed at time : $DATE"
