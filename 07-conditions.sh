a4fbba8bb31ac50c64c82fc6caffd8603b5e9d79
vikram Reddy <vikram@gmail.com>
2023-10-05 14:41:24 -0400

    updated conversation  Ramesh and suresh

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -2,7 +2,7 @@
 # check if number greater than 10 or not
 
 num=$1
-if($num -gt 10)
+if[$num -gt 10]
 then
     echo " number is greater than 10"
 else
---
+++ 08-install-git.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# our program goal is to install mysql
+
+USERID=$(id -u)
+
+if[ $USERID -ne 0 ]
+then 
+    echo "ERROR: Please run this script with root access "
+    exit 1
+#else
+ #   echo  " INFO: You are Root user"
+fi
+
+yum install git -y
+
