7082fe4e09de5de569f834adbea5d3827ddeaee5
vikram Reddy <vikram@gmail.com>
2023-10-05 12:04:08 -0400

    updated conversation  Ramesh and suresh

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -5,8 +5,8 @@ PERSON2=$2
 
 echo "Hello World , I am learning DevOps with AWS"
 
-echo " Ramesh: Hi Suresh ! good morning"
-echo "Suresh : Good Morning!!"
-echo "Ramesh: How are you ?"
-echo " suresh: I am good.. How are you ?"
+echo " $PERSON1: Hi $PERSON2 ! good morning"
+echo "$PERSON2 : Good Morning!!"
+echo "$PERSON1: How are you ?"
+echo " $PERSON2: I am good.. How are you ?"
 echo "changed default branch"
