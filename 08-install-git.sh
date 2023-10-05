5878921e800a5328681073b0d0a2221b26552f00
vikram Reddy <vikram@gmail.com>
2023-10-05 14:52:46 -0400

    updated conversation  Ramesh and suresh

--- 08-install-git.sh
+++ 08-install-git.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+
 # our program goal is to install mysql
 
 USERID=$(id -u)
 
-if[ $USERID -ne 0 ]
-then 
-    echo "ERROR: Please run this script with root access "
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
     exit 1
-#else
- #   echo  " INFO: You are Root user"
+# else
+#     echo "INFO:: You are root user"
 fi
 
-yum install git -y
+# it is our responsibility again to check installation is success or not
+yum install mysql -y
 
