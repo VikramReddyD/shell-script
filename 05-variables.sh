585744cb0b6e7f5ccd9fa6829b8f0413cf1b0502
vikram Reddy <vikram@gmail.com>
2023-10-05 12:51:46 -0400

    updated conversation  Ramesh and suresh

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,3 +5,6 @@ echo "Please enter your user name"
 read USERNAME # this is variable that user entered
 
 echo "Username entered by you is: $USERNAME "
+echo "Please enter your password"
+read -s Password # this is variable that user entered
+echo "Password entered by you is: $Password "
