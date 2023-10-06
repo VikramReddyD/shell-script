22d3e5f6b453252bba72783b2ef958733fd81ede
vikram Reddy <vikram@gmail.com>
2023-10-06 16:35:54 -0400

    updated conversation  Ramesh and suresh

--- 15-delete-old-log-files.sh
+++ 15-delete-old-log-files.sh
@@ -4,3 +4,8 @@ SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name -ls -o -regex '.*\.log' -type f -mtime +30)
 echo "$FILES_TO_DELETE"
+while read line
+ do
+    echo "Deleting $line" &> $LOGFILE
+    rm -rf $line
+ done < $FILES_TO_DELETE
