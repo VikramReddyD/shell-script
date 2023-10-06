bd6351377a157741bf80fdec926617ff27ab2bc3
vikram Reddy <vikram@gmail.com>
2023-10-06 09:59:54 -0400

    updated conversation  Ramesh and suresh

--- 09-functions.sh
+++ 09-functions.sh
@@ -6,8 +6,8 @@ DATE=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 USERID=$(id -u)
-R="\e[32m"
-G="\e[31m"
+R="\e[31m"
+G="\e[32m"
 N="\e[0m"
 #This function should validate the previous command and inform the user if it is success or not
 VALIDATE(){
