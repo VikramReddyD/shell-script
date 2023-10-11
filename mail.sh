898f0ded794c01df56ab5b0b726a87a8c1624bca
vikram Reddy <vikram@gmail.com>
2023-10-11 05:41:10 -0400

    disk usage and email

--- mail.sh
+++ mail.sh
@@ -5,4 +5,8 @@ BODY=$3
 TEAM_NAME=$4
 ALERT_TYPE=$5
 
-echo "all args: $@"
+#echo "all args: $@"
+
+FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/g" template.html)
+
+echo "$FINAL_BODY" | mail -s "$SUBJECT" $To_ADDRESS
